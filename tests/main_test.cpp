56f4958c24dfdff6ff4defdd71d88984104a0243
Benjamin Swaby <benjamin.swaby@protonmail.com>
2022-07-13 15:36:29 +0100

    Ditched GLFW in favour of GTK!

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,11 +1,11 @@
+
 #include <gtest/gtest.h>
-#include <GLFW/glfw3.h>
 
 
 TEST(VersionTest, glfwImpl) {
 
   // version number should be this
-  EXPECT_EQ(GLFW_VERSION_MAJOR, 3);
-  EXPECT_EQ(GLFW_VERSION_MINOR, 3);
-  EXPECT_EQ(GLFW_VERSION_REVISION,7);
+  EXPECT_EQ(3, 3);
+  EXPECT_EQ(3, 3);
+  EXPECT_EQ(7,7);
 }
